0ce1ae6043abe47b80c8beeea638c77a48ac37fc
unknown <daffarizkyaqilah@gmail.com>
2024-11-29 12:57:45 +0700

    add crud api

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
     protected $guarded = ['created_at'];
+
+    public function image(): Attribute
+    {
+        return Attribute::make(get: fn($value) => url('/storage/post/' . $value));
+    }
 }
